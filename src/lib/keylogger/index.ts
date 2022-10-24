816344704d7e46ff166eeb2ecd14fb6ff293bc8a
Paulo Marinho <pakas_vieira_95@hotmail.com>
2022-10-24 15:22:49 -0300

    refactor(keylogger): improve code quality

--- src/lib/keylogger/index.ts
+++ src/lib/keylogger/index.ts
@@ -6,35 +6,41 @@ import toKey from "./keycodes";
 const EVENT_TYPES = ["up", "down", "hold"];
 const EV_KEY = 1;
 
+interface KeyEvent {
+    timeS: number;
+    timeMS: number;
+    keyCode: number;
+    keyId: string;
+    type: string;
+    device: string;
+}
+
 class Keyboard extends EventEmitter {
-    // Por algum motivo esse cara buga o build:
-    dev: string;
-    bufferSize: number;
-    buffer: Buffer;
-    data: fs.ReadStream;
+    private readonly device: string;
+    private readonly bufferSize: number;
+    private readonly buffer: Buffer;
+    private readonly data: fs.ReadStream;
 
     constructor(dev: string) {
         super();
-        this.dev = dev || "event0";
+        this.device = dev || "event0";
         this.bufferSize = 24;
-        // Testar:
-        // this.buffer = Buffer.alloc(this.bufferSize);
-        this.buffer = new Buffer(this.bufferSize);
-        this.data = fs.createReadStream("/dev/input/".concat(this.dev));
+        this.buffer = Buffer.alloc(this.bufferSize);
+        this.data = fs.createReadStream("/dev/input/".concat(this.device));
         this.onRead();
-        // this.emit("ready");
     }
-    onRead() {
+
+    onRead(): void {
         // eslint-disable-next-line @typescript-eslint/no-this-alias
         const self = this;
 
         this.data.on("data", function (data) {
             this.buffer = data.slice(24);
-            const event = self.parse(this, this.buffer);
-            if (event) {
-                event.dev = self.dev;
-                self.emit(event.type, event);
+            if (this.buffer.readUInt16LE(16) !== EV_KEY) {
+                return;
             }
+            const event = self.parse(this.buffer);
+            self.emit(event.type, event);
         });
 
         this.data.on("error", function (err) {
@@ -43,65 +49,16 @@ class Keyboard extends EventEmitter {
         });
     }
 
-    parse(input, buffer) {
-        let event;
-        if (buffer.readUInt16LE(16) === EV_KEY) {
-            event = {
-                timeS: buffer.readUInt16LE(0),
-                timeMS: buffer.readUInt16LE(8),
-                keyCode: buffer.readUInt16LE(18),
-            };
-            event.keyId = toKey[event.keyCode];
-            event.type = EVENT_TYPES[buffer.readUInt32LE(20)];
-        }
-        return event;
+    parse(buffer: Buffer): Partial<KeyEvent> {
+        return {
+            timeS: buffer.readUInt16LE(0),
+            timeMS: buffer.readUInt16LE(8),
+            keyCode: buffer.readUInt16LE(18),
+            keyId: toKey[buffer.readUInt16LE(18)],
+            type: EVENT_TYPES[buffer.readUInt32LE(20)],
+            device: this.device,
+        };
     }
 }
 
-// function Keyboard(dev) {
-//     this.dev = dev || "event0";
-//     this.bufferSize = 24;
-//     this.buffer = new Buffer(this.bufferSize);
-//     this.data = fs.createReadStream("/dev/input/".concat(this.dev));
-//     this.onRead();
-// }
-
-// Keyboard.prototype = Object.create(EventEmitter.prototype, {
-//     constructor: { value: Keyboard },
-// });
-
-// Keyboard.prototype.onRead = function onRead() {
-//     // eslint-disable-next-line @typescript-eslint/no-this-alias
-//     const self = this;
-
-//     this.data.on("data", function (data) {
-//         this.buffer = data.slice(24);
-//         const event = parse(this, this.buffer);
-//         if (event) {
-//             event.dev = self.dev;
-//             self.emit(event.type, event);
-//         }
-//     });
-
-//     this.data.on("error", function (err) {
-//         self.emit("error", err);
-//         throw new Error(err);
-//     });
-// };
-
-// function parse(input, buffer) {
-//     let event;
-//     if (buffer.readUInt16LE(16) === EV_KEY) {
-//         event = {
-//             timeS: buffer.readUInt16LE(0),
-//             timeMS: buffer.readUInt16LE(8),
-//             keyCode: buffer.readUInt16LE(18),
-//         };
-//         event.keyId = toKey[event.keyCode];
-//         event.type = EVENT_TYPES[buffer.readUInt32LE(20)];
-//     }
-//     return event;
-// }
-
-// Keyboard.Keys = toKey;
 export default Keyboard;

cb789dd4db11ec6b39a3be3c60e8f74fc5d5f426
Paulo Marinho <pakas_vieira_95@hotmail.com>
2022-10-28 08:50:05 -0300

    fix: fix lint issues

--- src/utils/shortcuts-file-parser.ts
+++ src/utils/shortcuts-file-parser.ts
@@ -1,4 +1,4 @@
-import { ShortcutsFile, Shortcut, Key } from "../types/shortcut";
+import { Key, Shortcut, ShortcutsFile } from "../types/shortcut";
 import { KeyComparatorUtil } from "./key-comparator";
 
 export class ShortcutsFileUtil {
@@ -14,38 +14,50 @@ export class ShortcutsFileUtil {
         const parsedShortcuts: Shortcut[] = [];
         for (const shortcut of shortcutsOnFile) {
             const trigger = this.parseTrigger(shortcut?.trigger || []);
-            const actions = this.parseActions(shortcut?.actions || [] as Shortcut["actions"]);
-            if (!trigger?.length || !actions?.length) continue;
+            const actions = this.parseActions((shortcut?.actions || []) as Shortcut["actions"]);
+            if (!trigger?.length || !actions?.length) {
+                continue;
+            }
             parsedShortcuts.push({ trigger, actions });
         }
         return parsedShortcuts;
     }
 
+    // eslint-disable-next-line complexity
     private static parseActions(actionsOnFile: Shortcut["actions"]): Shortcut["actions"] {
         const parsedActions: Shortcut["actions"] = [];
-        for (const actions of actionsOnFile) {
-            const { actionType, content, keys } = actions;
-            if (!actionType) continue;
-            if (!content && (!keys || !Array.isArray(keys) || !keys.length)) continue;
-            if (keys && keys.some((el) => !KeyComparatorUtil.isValidKey(el))) continue;
+        for (const action of actionsOnFile) {
+            const { actionType, content, keys } = action;
+            // TODO: Create 'isValidAction' util:
+            if (!actionType) {
+                continue;
+            }
+            if (!content && (!keys || !Array.isArray(keys) || !keys.length)) {
+                continue;
+            }
+            if (keys && keys.some((el) => !KeyComparatorUtil.isValidKey(el))) {
+                continue;
+            }
             parsedActions.push({ actionType, content, keys });
         }
         return parsedActions;
     }
 
     private static parseTrigger(
-        triggerOnFile: { keyId: string; clickType: string; }[]
+        triggerOnFile: { keyId: string; clickType: string }[],
     ): Shortcut["trigger"] {
         const parsedTrigger: Shortcut["trigger"] = [];
         for (const trigger of triggerOnFile) {
-            if (!KeyComparatorUtil.isValidKey(trigger as Key)) continue;
+            if (!KeyComparatorUtil.isValidKey(trigger as Key)) {
+                continue;
+            }
             const { clickType, keyId } = trigger;
             if (clickType === "tap") {
                 parsedTrigger.push({ clickType: "down", keyId });
                 parsedTrigger.push({ clickType: "up", keyId });
                 continue;
             }
-            parsedTrigger.push(({ clickType, keyId }) as Key);
+            parsedTrigger.push({ clickType, keyId } as Key);
         }
         return parsedTrigger;
     }

1176d06df60c7139cb158d7dd51a430f2cce8bfc
Paulo Marinho <pakas_vieira_95@hotmail.com>
2022-12-08 06:06:02 -0300

    fix: fix lint issues

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import Keyboard from "./lib/keylogger";
-import shortcutsFile from "../shortcuts.config.json";
-import ShortcutsManager from "./services/shortcuts-manager";
 import { ShortcutsFileUtil } from "./utils/shortcuts-file-parser";
+import ShortcutsManager from "./services/shortcuts-manager";
+import shortcutsFile from "../shortcuts.config.json";
 
-(function() {
+(function () {
     const shortcutsManager = new ShortcutsManager(
         ShortcutsFileUtil.parse(shortcutsFile),
         new Keyboard("event3")

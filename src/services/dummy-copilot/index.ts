f7c22acef37362b9f24255bcdf6c943c2e92303d
Paulo Marinho <pakas_vieira_95@hotmail.com>
2023-02-01 16:10:50 -0300

    refactor(dummy-copilot): improve interfaces imports

--- src/services/dummy-copilot/index.ts
+++ src/services/dummy-copilot/index.ts
@@ -1,19 +1,14 @@
 import * as ActionsMiddlewares from "./actions-middlewares";
+import * as Interfaces from "./interfaces";
 import ActionsExecutor from "./actions-executor";
 import Context from "./context";
-import { IActionsExecutor } from "./interfaces/i-actions-executor";
-import { IContext } from "./interfaces/i-context";
-import { IKey } from "./interfaces/i-key";
-import { IKeyEvent } from "./interfaces/i-key-event";
-import { IKeylogger } from "./interfaces/i-keylogger";
-import { IShortcut } from "./interfaces/i-shortcut";
 
 export default class DummyCopilot {
-    private shortcuts: IShortcut[];
-    private readonly devices: IKeylogger[];
-    private readonly context: IContext;
-    private keysClickedQueue: IKey<"down" | "up">[] = [];
-    private readonly actionsExecutor: IActionsExecutor;
+    private shortcuts: Interfaces.IShortcut[];
+    private readonly devices: Interfaces.IKeylogger[];
+    private readonly context: Interfaces.IContext;
+    private keysClickedQueue: Interfaces.IKey<"down" | "up">[] = [];
+    private readonly actionsExecutor: Interfaces.IActionsExecutor;
 
     constructor(devices: DummyCopilot["devices"]) {
         this.devices = devices;
@@ -27,8 +22,8 @@ export default class DummyCopilot {
     }
 
     startKeyListener(
-        onClickUpKey?: (_: IKeyEvent) => void,
-        onClickDownKey?: (_: IKeyEvent) => void,
+        onClickUpKey?: (_: Interfaces.IKeyEvent) => void,
+        onClickDownKey?: (_: Interfaces.IKeyEvent) => void,
     ): void {
         this.devices.forEach((keyboard) => {
             keyboard.on("up", onClickUpKey);
@@ -47,12 +42,7 @@ export default class DummyCopilot {
         this.shortcuts = shortcuts;
     }
 
-    // TODO: Handle this properly. Should be on actions-executor?
-    // setDelay(delayBetweenClicks: number): void {
-    //     robot.setKeyboardDelay(delayBetweenClicks);
-    // }
-
-    private onClickKey({ keyId, clickType }: IKeyEvent) {
+    private onClickKey({ keyId, clickType }: Interfaces.IKeyEvent) {
         this.keysClickedQueue.push({ keyId, clickType });
 
         const shortcut = this.shortcuts.find((element) =>
---
+++ src/services/dummy-copilot/interfaces/index.ts
@@ -0,0 +1,8 @@
+export { IShortcut } from "./i-shortcut";
+export { IKeylogger } from "./i-keylogger";
+export { IKeyEvent } from "./i-key-event";
+export { IKey } from "./i-key";
+export { IContext } from "./i-context";
+export { IActionsExecutor } from "./i-actions-executor";
+export { IActionMiddleware } from "./i-action-middleware";
+export { IAction } from "./i-action";
